54830270a4878c6b3ce1a61339049f106d83c43b
HanbitGaram(ハンビッガラム) <github.external@netko.kr>
2023-03-06 12:27:06 +0900

    Add docker update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+docker compose build web
+docker compose exec web yarn cache clean
+docker compose exec web yarn build:production
+docker compose run --rm web rails assets:precompile
+docker compose exec web yarn build:production
+docker compose up -d
+docker compose exec web yarn cache clean
+docker compose exec web yarn build:production
+docker compose run --rm web rails assets:precompile
+docker compose exec web yarn build:production
+docker compose up -d
