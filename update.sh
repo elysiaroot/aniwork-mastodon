b8350b40a9bae8b41968f073055929ee8d8c3d89
HanbitGaram(ハンビッガラム) <github.external@netko.kr>
2023-09-09 21:07:32 +0900

    Some Options Added

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-docker compose build web
+docker compose build
 docker compose exec web yarn cache clean
 docker compose exec web yarn build:production
 docker compose run --rm web rails assets:precompile
 docker compose exec web yarn build:production
 docker compose up -d
+
 docker compose exec web yarn cache clean
 docker compose exec web yarn build:production
 docker compose run --rm web rails assets:precompile
