d0b4d40dbe44dceabd85eb3e7cc006c51b6881f3
HanbitGaram(ハンビッガラム) <github.external@netko.kr>
2023-11-05 00:59:52 +0900

    [Rebase] Update version 4.2.1

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -9,19 +9,23 @@ module Mastodon
     end
 
     def minor
-      1
+      2
     end
 
     def patch
-      7
+      1
     end
 
-    def flags
-      '+aniwork'
-    end
-
-    def suffix
+    def default_prerelease
       ''
+    end
+
+    def prerelease
+      ENV['MASTODON_VERSION_PRERELEASE'].presence || default_prerelease
+    end
+
+    def build_metadata
+      ENV.fetch('MASTODON_VERSION_METADATA', nil)
     end
 
     def to_a
@@ -29,11 +33,18 @@ module Mastodon
     end
 
     def to_s
-      [to_a.join('.'), flags, suffix].join
+      components = [to_a.join('.')]
+      components << "-#{prerelease}" if prerelease.present?
+      components << "+#{build_metadata}" if build_metadata.present?
+      components.join
+    end
+
+    def gem_version
+      @gem_version ||= Gem::Version.new(to_s.split('+')[0])
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'elysiaroot/aniwork-mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
     end
 
     def source_base_url
